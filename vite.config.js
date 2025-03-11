a7f194fc76c9a7c64bebdeb10735b2e4bd96ffbd
zohaibyounas <zohaiby737@gmail.com>
2025-03-11 10:49:00 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Ensure correct base path
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Split vendor dependencies
+          }
+        },
+      },
+    },
+  },
+});
